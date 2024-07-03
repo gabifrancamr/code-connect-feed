f926aacaf4aef77bc207b66e6c2ca0e47167481e
Gabriela <gabifmouraroc@outlook.com>
2024-07-03 09:36:05 -0300

    action increment thumps up

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -6,26 +6,21 @@ import Link from "next/link";
 
 const CardPost = ({ post, highlight }) => {
   return (
-    <Link href={`/posts/${post.slug}`} className={styles.link} >
-      <article className={styles.card} style={{ width: highlight ? 993 : 486}}>
-        <header className={styles.header}>
-          <figure style={{ height: highlight ? 300 : 133}}>
-            <Image
-              src={post.cover}
-              fill
-              alt={`Capa do post: ${post.title}`}
-            />
-          </figure>
-        </header>
-        <section className={styles.body}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </section>
-        <footer className={styles.footer}>
-          <Avatar imageSrc={post.author.avatar} name={post.author.username} />
-        </footer>
-      </article>
-    </Link>
+    <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
+      <header className={styles.header}>
+        <figure style={{ height: highlight ? 300 : 133 }}>
+          <Image src={post.cover} fill alt={`Capa do post: ${post.title}`} />
+        </figure>
+      </header>
+      <section className={styles.body}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <Link href={`/posts/${post.slug}`} className={styles.link}>Ver detalhes</Link>
+      </section>
+      <footer className={styles.footer}>
+        <Avatar imageSrc={post.author.avatar} name={post.author.username} />
+      </footer>
+    </article>
   );
 };
 
